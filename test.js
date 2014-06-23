2a5c4ffa029f8a5b1e161db732b8fc4720ea10d9
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-06-23 14:13:54 -0400

    Automated commit. [ci skip]

--- test.js
+++ test.js
@@ -1,25 +1,42 @@
 module.exports = {  
-  'scaffold-wp-plugin': { 
-    'has valid project.yml file.': require( 'grunt-scaffold-module' ).testProjectValidity,
-    'has valid project structure.': require( 'grunt-scaffold-module' ).testStructure,
-    'has valid public methods.': require( 'grunt-scaffold-module' ).testMethods,
-    'has valid public classess.': require( 'grunt-scaffold-module' ).testClasses,
+  'Valid project': { 
     
-    'Unit Tests': {
-      
-      'phpUnit': require( 'grunt-scaffold-module' ).phpUnit({
-        
-      }),
+    'has valid project-yml file.': require( 'grunt-scaffold-module' ).testProjectValidity({
+      debug: false
+    }),
+    
+    'has valid project-yml structure.': require( 'grunt-scaffold-module' ).testStructure({
+      debug: false      
+    })
+    
+  },
 
-      'nodeUnit': require( 'grunt-scaffold-module' ).nodeUnit({
-        
-      }),
+  'Valid API': {
 
-      'anotherTest': function() {
-        console.log({'asdf': "asdf"});
-      }
-      
+    'has expected methods': require( 'grunt-scaffold-module' ).testMethods({
+      debug: false
+    }),
+
+    'has valid public classess.': require( 'grunt-scaffold-module' ).testClasses({
+      debug: false
+    })
+
+  },
+
+  'Unit tests': {    
+
+    'phpUnit': require( 'grunt-scaffold-module' ).phpUnit({
+      debug: false
+    }),
+
+    'nodeUnit': require( 'grunt-scaffold-module' ).nodeUnit({
+      debug: false
+    }),
+
+    'custom test': function customTest() {  
+      console.log({'asdf': "asdf"});
     }
+    
   }
-  
 }
+
