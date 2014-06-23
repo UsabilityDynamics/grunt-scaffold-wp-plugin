9696087eb5ee69d9e705445f328979b4f12ca3d8
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-06-23 13:29:51 -0400

    Automated commit. [ci skip]

--- test.js
+++ test.js
@@ -3,6 +3,23 @@ module.exports = {
     'has valid project.yml file.': require( 'grunt-scaffold-module' ).testProjectValidity,
     'has valid project structure.': require( 'grunt-scaffold-module' ).testStructure,
     'has valid public methods.': require( 'grunt-scaffold-module' ).testMethods,
-    'has valid public classess.': require( 'grunt-scaffold-module' ).testClasses        
-  } 
+    'has valid public classess.': require( 'grunt-scaffold-module' ).testClasses,
+    
+    'Unit Tests': {
+      
+      'phpUnit': require( 'grunt-scaffold-module' ).phpUnit({
+        
+      }),
+
+      'nodeUnit': require( 'grunt-scaffold-module' ).nodeUnit({
+        
+      }),
+
+      'anotherTest': function() {
+        console.log({'asdf': "asdf"});
+      }
+      
+    }
+  }
+  
 }
