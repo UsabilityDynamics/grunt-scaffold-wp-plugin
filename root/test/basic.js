4016013b8223872fb33c1940c054f88969f708d1
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-06-25 09:19:54 -0400

    Update basic.js

--- root/test/basic.js
+++ root/test/basic.js
@@ -3,6 +3,9 @@ module.exports = {
     'has valid project.yml file.': require( 'grunt-scaffold-module' ).testProjectValidity(),
     'has valid project structure.': require( 'grunt-scaffold-module' ).testStructure(),
     'has valid public methods.': require( 'grunt-scaffold-module' ).testMethods(),
-    'has valid public classess.': require( 'grunt-scaffold-module' ).testClasses()        
+    'has valid public classess.': require( 'grunt-scaffold-module' ).testClasses(),
+    'phpUnit validates..': require( 'grunt-scaffold-module' ).phpUnit({
+      
+    })        
   } 
 }
